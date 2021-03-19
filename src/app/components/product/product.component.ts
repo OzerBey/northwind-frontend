f305dc8dbb2f7399f66849b3b670679a9b15ea70
OzerBey <ysnozer.zer@gmail.com>
2021-03-19 15:44:43 +0300

    ana kontroller yapıldı liste yapısı temeli oluşturulup çalışır hale getirildi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,53 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; //bununla birlikte backende istekte bulunabiliyoruz
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  product1 = {
-    productId: 1,
-    productName: 'Glass',
-    categoryId: 1,
-    unitPrice: 15,
-  };
-  product2 = {
-    productId: 2,
-    productName: 'Notebook',
-    categoryId: 2,
-    unitPrice: 1500,
-  };
-  product3 = {
-    productId: 3,
-    productName: 'Pen',
-    categoryId: 2,
-    unitPrice: 15,
-  };
-  product4 = {
-    productId: 4,
-    productName: 'Book',
-    categoryId: 2,
-    unitPrice: 150,
-  };
-  product5 = {
-    productId: 5,
-    productName: 'Plate',
-    categoryId: 1,
-    unitPrice: 35,
-  };
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
-
-  constructor() { }
+  products: Product[] = [];
+  apiUrl = 'http://localhost:50630/api/products/getall';
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
+    console.log('Init çalıştı');
+    this.getProducts();
   }
 
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl) //apiUrl 'den gelen datayı ProductResponseModel e map edecegini belirtiyoruz
+      .subscribe((response) => {
+        this.products = response.data;
+      });
+  }
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  productId: number;
+  categoryId: number;
+  productName: string;
+  quantityPerUnit: number;
+  unitsInStock: number;
+  unitPrice: number;
+}
