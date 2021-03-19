1b3773aaf508aff8db8f8e9b7b662617347baac8
OzerBey <ysnozer.zer@gmail.com>
2021-03-19 15:12:16 +0300

    Ürüm listeleme ve genel temel görünüm elde edildi

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+//export :public
+export interface Product {
+  productId: number;
+  categoryId: number;
+  productName: string;
+  quantityPerUnit: string;
+  unitsInStock: number;
+  unitPrice: number;
+}
